17dd8b95a6cf644fc1fa107ef9cf58c152bfb90d
Paul Kretschel <paul.kretschel@icloud.com>
2021-02-15 12:04:40 +0100

    Simplify code of development theme

--- app/functions.php
+++ app/functions.php
@@ -3,8 +3,28 @@
 require_once get_template_directory() . '/vendor/autoload.php';
 require_once dirname(dirname(get_template_directory())) . '/wpdev/vendor/autoload.php';
 
-Src\Entities\LandingPage::register();
+use WPDev\Theme;
 
-WPDev\Theme::init(new WPDev\Enqueue('WPDev'));
+$landing_page_template_path = 'templates/landing-page.php';
+
+Theme::register_page_template($landing_page_template_path, [
+  'disable_editor' => true,
+]);
+
+Theme::register_activation_callback(function () use ($landing_page_template_path) {
+  $pid = Theme::create_page('landing-page', 'WPDev', 'This is the landing page.', $landing_page_template_path, []);
+
+  if (!\get_option('page_on_front')) {
+    \update_option('page_on_front', $pid);
+    \update_option('show_on_front', 'page');
+  }
+
+  if (!\get_post_thumbnail_id($pid)) {
+    $id = \WPDev\Util\ThemeMedia::get_id('static/sample.png');
+    \set_post_thumbnail($pid, $id);
+  }
+});
+
+Theme::init(new WPDev\Enqueue('WPDev'));
 
 add_theme_support('post-thumbnails');
