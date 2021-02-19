9cbf4548365212c4ebaaf0ef762a928e082d9d73
Paul Kretschel <paul.kretschel@icloud.com>
2021-02-19 13:20:13 +0100

    Support retrieving front end assets from PHP

--- src/Enqueue.php
+++ src/Enqueue.php
@@ -7,7 +7,8 @@ class Enqueue
   public function __construct(string $app_name, string $entry_group = 'app', string $output_path = 'dist')
   {
     $this->entry_group = $entry_group;
-    $this->enqueue = new \WPackio\Enqueue($app_name ?? 'app', $output_path, null, 'theme');
+    $this->enqueue = new \WPackio\Enqueue($app_name, $output_path, null, 'theme');
+    $this->manifests = [];
     $this->entries = [];
 
     \add_action('wp_enqueue_scripts', function () {
@@ -32,5 +33,16 @@ class Enqueue
   function enqueue(string $entry, $entry_group = null)
   {
     $this->enqueue->enqueue($entry_group ?? $this->entry_group, $entry, []);
+  }
+
+  function get_asset_by_filename($filename, $entry_group = null)
+  {
+    if (!$entry_group) $entry_group = $this->entry_group;
+    @$manifest = $this->manifests[$entry_group];
+
+    if (!$manifest)
+      $manifest = $manifests[$entry_group] = $this->enqueue->get_manifest($entry_group);
+
+    return @$manifest["$this->entry_group/assets/$filename"];
   }
 }
