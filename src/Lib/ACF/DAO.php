82b417087cb0271bbcf0709e718b755649ef09fe
Paul Kretschel <paul.kretschel@icloud.com>
2021-09-02 18:47:39 +0200

    Support options pages in ACF DAO

--- src/Lib/ACF/DAO.php
+++ src/Lib/ACF/DAO.php
@@ -4,32 +4,35 @@ namespace WPDev\Lib\ACF;
 
 class DAO
 {
-  private $post;
+  private $entity;
   private $entity_name;
   private $acf_inactive;
 
-  function __construct(\WP_Post $post)
+  function __construct($entity)
   {
-    $this->entity_name = $post->post_type === 'page'
-      ? Page::template_path_to_name(
-        get_page_template_slug($post)
-      )
-      : $post->post_type;
+    $is_options = is_string($entity);
 
-    $this->post = $post;
+    $this->entity_name = $is_options
+      ? $entity : ($entity->post_type === 'page'
+        ? Page::template_path_to_name(
+          get_page_template_slug($entity)
+        )
+        : $entity->post_type);
+
+    $this->entity = $is_options ? 'options' : $entity;
     $this->acf_inactive = !Util::acf_active();
   }
 
   function get($key_value, $format_value = true)
   {
     if ($this->acf_inactive || !$key_value) return false;
-    return \get_field($this->parse_key($key_value), $this->post, $format_value);
+    return \get_field($this->parse_key($key_value), $this->entity, $format_value);
   }
 
   function update($key_value, $value)
   {
     if ($this->acf_inactive || !$value) return false;
-    return \update_field($this->parse_key($key_value), $value, $this->post);
+    return \update_field($this->parse_key($key_value), $value, $this->entity);
   }
 
   private function parse_key($value)
